7ef802fd82c43f5aee0c6311794b2c2175e36437
Jane'space <99324072+Jane-space@users.noreply.github.com>
2024-02-24 14:40:11 +0300

    Create user_input.py
    first assignment

---
+++ user_input.py
@@ -0,0 +1,12 @@
+#user_input.py
+# Ask the user's name 
+name = input("Enter your name: ")
+
+# Ask for the age 
+age = input("Enter your age: ")
+
+# Ask the user for their location 
+location = input("Enter your location: ")
+
+# Print out 
+print(f"Hello {name}, you are {age} years old and live in {location}.")
